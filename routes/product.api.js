d4c47a50c2aaeac524964df07454ec903025a82f
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-08 23:15:18 +0900

    Update product.api.js

--- routes/product.api.js
+++ routes/product.api.js
@@ -11,16 +11,4 @@ router.post('/',
 
 router.get('/', productController.getProducts)
 
-router.put('/:id', 
-    authController.authenticate, 
-    authController.checkAdminPermission, 
-    productController.updateProduct
-)
-
-router.delete('/:id', 
-    authController.authenticate, 
-    authController.checkAdminPermission, 
-    productController.deleteProduct
-)
-
 module.exports = router;
