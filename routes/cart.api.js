fb1093f83e6f31d8e8cf7d90cfe3932287923f07
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-09 01:20:58 +0900

    Update cart.api.js

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 const cartController = require('../controllers/cart.controller');
 
-router.post('/', cartController.addToCart);
+router.post('/', 
+    authController.authenticate, 
+    cartController.addItemToCart
+);
 
 module.exports = router;
