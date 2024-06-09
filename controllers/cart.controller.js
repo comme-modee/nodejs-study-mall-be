47398b8538fc1b659560381da7b623bc76a0b034
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-09 21:03:31 +0900

    Update cart.controller.js

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -91,4 +91,15 @@ cartController.editCartItem = async (req, res) => {
     }
 };
 
+cartController.getCartQty = async (req, res) => {
+    try {
+      const { userId } = req;
+      const cart = await Cart.findOne({ userId: userId });
+      if (!cart) throw new Error('카트가 없습니다.');
+      res.status(200).json({ status: 'success', qty: cart.items.length });
+    } catch (error) {
+      return res.status(400).json({ status: 'fail', error: error.message });
+    }
+};
+
 module.exports = cartController;
